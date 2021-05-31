73d9856f17a3fd584ea9c40639de36bf7bb204af
JavierAmaya <photojavhn@gmail.com>
2021-05-31 01:07:41 -0600

    adding dependencys

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+
 import "./css/main.css";
+
+/// Aqui vamos a exportar todas las vistas y se va renderizar segun la ruta (path).
 import Example from './views/example';
 
 const App: React.FC = () => (
 
-  <div>
-    <Example></Example>
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/example" exact component = {Example} />
+    
+    </Switch>
+
+  </BrowserRouter>
  
 )
 
